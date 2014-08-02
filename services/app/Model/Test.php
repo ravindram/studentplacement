55e7560dbfad946ff6d0b574c9985777ea3fdf3e
ravindram <ravindranath@mantralabsglobal.com>
2014-08-02 18:28:02 +0530

    fixed conflict

--- services/app/Model/Test.php
+++ services/app/Model/Test.php
@@ -1,12 +1,5 @@
 <?php
 
 class Test extends AppModel{
-<<<<<<< HEAD
-	public $hasMany = array('MyTestQuestion' => array('className' => 'TestQuestion'));
-=======
-	public $hasMany = array('TestQuestion','CandidateTest','CandidateTestResponse');
-	
-	
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
+	public $hasMany = array('TestQuestion','CandidateTest','CandidateTestResponse');	
 }
-?>
