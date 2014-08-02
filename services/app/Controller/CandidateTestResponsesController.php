a52a20d990d73d5cbd017eefad9f93a9ae731631
ravindram <ravindranath@mantralabsglobal.com>
2014-08-02 18:07:39 +0530

    fixed conflict

--- services/app/Controller/CandidateTestResponsesController.php
+++ services/app/Controller/CandidateTestResponsesController.php
@@ -1,28 +1,20 @@
 <?php
 class CandidateTestResponsesController extends AppController{
-<<<<<<< HEAD
-    var $uses = array('Question','TestQuestion', 'Test','CandidateTestResponse');
-=======
     var $uses = array('Question','TestQuestion', 'Test','CandidateTestResponse', 'CandidateTest');
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
+    
     public function beforeFilter() {
         parent::beforeFilter();
         $this->Auth->allow('add','index');
     }
 
     public function index() {
-<<<<<<< HEAD
-        $data=$this->Auth->user();
-        print_r($data['email']); die;
-        $candidatetestresponse = $this->CandidateTestResponse->find('all');
-=======
         $candidatetestresponse = $this->CandidateTestResponse->find('all',array('fields'=>array('id','candidate_id','test_id','question_id','response')));
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
         $this->set(array(
             'candidatetestresponse' => $candidatetestresponse,
             '_serialize' => array('candidatetestresponse')
         ));   
     }
+
     public function view($id) {
         $candidatetestresponse = $this->CandidateTestResponse->findById($id);
         $this->set(array(
@@ -30,45 +22,8 @@ class CandidateTestResponsesController extends AppController{
             '_serialize' => array('CandidateTestResponseCandidateTestResponse')
         ));
     }
+
     public function add() {
-<<<<<<< HEAD
-        $data=$this->request->input('json_decode',true);
-        print_r($data[0]['test_id']); 
-        $id=$data[0]['test_id'];
-        $response=array();
-        $questions_id=array();
-        $answer=array();
-        foreach ($data as $key => $value) {
-            array_push($response,$value['RESPONSE']);
-        }
-        print_r($response);
-        $con=mysqli_connect("localhost","root","","placementapp_db");
-        $result = mysqli_query($con,"SELECT * FROM test_questions
-        WHERE test_id=$id");
-        while($row = mysqli_fetch_array($result)) {
-            array_push($questions_id,$row['question_id']);
-        }
-        print_r($questions_id);
-        for ($i=0; $i <count($questions_id) ; $i++) {
-            $con=mysqli_connect("localhost","root","","placementapp_db"); 
-            $results = mysqli_query($con,"SELECT * FROM questions
-            WHERE id=$questions_id[$i]");
-            while($row = mysqli_fetch_array($results)) {
-                array_push($answer,$row['answer']);
-            }
-        }
-        print_r($answer);
-        $r=0;
-        for ($j=0; $j <count($answer) ; $j++) { 
-            if($response[$j]==$answer[$j]){
-                $r++;
-            }
-        }
-        print_r($r); die;
-        $this->set(array(
-        'testQuestion' => $testQuestion,
-        '_serialize' => array('TestQuestion')
-=======
        $data=$this->Auth->user();
         $user_id=$data['id'];
         $data=$this->request->input('json_decode',true);
@@ -114,7 +69,6 @@ class CandidateTestResponsesController extends AppController{
         $this->set(array(
         'candidatetestresponse' => $candidatetestresponse,
         '_serialize' => array('candidatetestresponse')
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
         ));
     }
 
@@ -145,4 +99,3 @@ class CandidateTestResponsesController extends AppController{
         ));
     }
 }
-?>
