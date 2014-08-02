db284ab36993af94c9f8dd0b185be9ae7009c36e
ravindram <ravindranath@mantralabsglobal.com>
2014-08-02 18:11:09 +0530

    fixed conflict

--- services/app/Controller/CollegesController.php
+++ services/app/Controller/CollegesController.php
@@ -1,33 +1,6 @@
 <?php
 class CollegesController extends AppController{
 	var $uses = array('User','College','Candidate');
-<<<<<<< HEAD
-	public function beforeFilter() {
-	    parent::beforeFilter();
-	    $this->Auth->allow('add','index', 'login', 'logout');
-	}
-	public function login() {
-	    if ($this->request->is('post')) {
-	        if ($this->Auth->login()) {
-	            $this->Auth->logout();
-	        }
-	    }
-	}
-    public function logout() {
-    $this->Auth->logout();
-
-    }   
-	public function index(){
-		$colleges = $this->College->find('all');
-		$this->set(array('colleges' => $colleges, '_serialize' => array('colleges')));
-	}
-
-	public function view($id) {
-		$college = $this->College->findById($id);
-		$this->set(array('college' =>$college, '_serialize' => array('college')));
-	}
-	public function add() {
-=======
 	
 	public function beforeFilter() {
 	    parent::beforeFilter();
@@ -48,7 +21,6 @@ class CollegesController extends AppController{
 		$this->set(array('college' =>$college, '_serialize' => array('college')));
 	}
 	public function admin_add() {
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
 		$data=$this->request->input('json_decode',true);
         $college=$data["College"];
         $user=$data['User'];
@@ -57,10 +29,7 @@ class CollegesController extends AppController{
 	        $id=$this->User->getLastInsertId(); 
 	        $this->College->create();
 	        $college['user_id']=$id;
-<<<<<<< HEAD
-=======
 	        $college['college']=1;
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
 	        $this->College->save($college);
 	        $message = $this->User->findById($id);
     	}
@@ -70,11 +39,7 @@ class CollegesController extends AppController{
 		$this->set(array('message' => $message, '_serialize' => array('message')));
 	}
 
-<<<<<<< HEAD
-	public function edit($id) {
-=======
 	public function admin_edit($id) {
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
 		$this->College->id = $id;
 		if ($this->College->save($this->request->data)) {
 			$message = array('text' => _('Saved'), 'type' => 'success');
@@ -85,11 +50,7 @@ class CollegesController extends AppController{
 		$this->set(array('message' => $message, '_serialize' => array('message')));
 	}
 
-<<<<<<< HEAD
-	public function delete($id) {
-=======
 	public function admin_delete($id) {
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
 		if ($this->College->delete($id)) {
 			$message = array('text' => _('Deleted'), 'type' =>'success');
 		} 
