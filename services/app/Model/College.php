87d3b6fd9edb9bb8f98065dfa3c74899a0a176b9
ravindram <ravindranath@mantralabsglobal.com>
2014-08-02 18:31:20 +0530

    fixed conflict

--- services/app/Model/College.php
+++ services/app/Model/College.php
@@ -2,12 +2,7 @@
 
 class College extends AppModel{
 	public $belongsTo = array('User');
-<<<<<<< HEAD
-	public $hasMany = array('MyCandidate' => array('className' => 'Candidate'));
-=======
 	public $hasMany = array('Candidate');
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
 	public $validate = array('COLLEGE_NAME' => array('rule' => 'notEmpty'));
 
 }
-?>
