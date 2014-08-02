522f00231c6f119c04d805f459e827626144c53b
ravindram <ravindranath@mantralabsglobal.com>
2014-08-02 18:25:16 +0530

    fixed conflict

--- services/app/Model/Question.php
+++ services/app/Model/Question.php
@@ -1,11 +1,6 @@
 <?php
 
 class Question extends AppModel{
-<<<<<<< HEAD
-	public $hasMany = array('TestQuestion');
-=======
 	public $belongsTo = array('Category');
 	public $hasMany = array('TestQuestion','CandidateTestResponse');
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
 }
-?>
