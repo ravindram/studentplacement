407c760cefe3207e08c8fb6d111c6ceda06671b9
ravindram <ravindranath@mantralabsglobal.com>
2014-08-02 18:26:54 +0530

    fixed conflict

--- services/app/Model/CandidateTestResponse.php
+++ services/app/Model/CandidateTestResponse.php
@@ -1,10 +1,5 @@
 <?php
 
 class CandidateTestResponse extends AppModel{
-<<<<<<< HEAD
-
-=======
 	public $belongsTo = array('Candidate','Test','Question');
->>>>>>> 4c2649b073355a9ed3289f416176bca4d813ad11
 }
-?>
