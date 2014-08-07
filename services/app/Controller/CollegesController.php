84b3ea40dc5052f6f5e861fcd0aa048a08170186
ravindram <ravindranath@mantralabsglobal.com>
2014-08-07 13:19:05 +0530

    message

--- services/app/Controller/CollegesController.php
+++ services/app/Controller/CollegesController.php
@@ -39,7 +39,7 @@ class CollegesController extends AppController{
 		$this->set(array('message' => $message, '_serialize' => array('message')));
 	}
 
-	public function edit($id) {
+	public function admin_edit($id) {
 		$this->College->id = $id;
 		if ($this->College->save($this->request->data)) {
 			$message = array('text' => _('Saved'), 'type' => 'success');
@@ -50,7 +50,7 @@ class CollegesController extends AppController{
 		$this->set(array('message' => $message, '_serialize' => array('message')));
 	}
 
-	public function delete($id) {
+	public function admin_delete($id) {
 		if ($this->College->delete($id)) {
 			$message = array('text' => _('Deleted'), 'type' =>'success');
 		} 
